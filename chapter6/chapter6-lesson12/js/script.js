bec8a058d1a8bb94d4cbea25103c2c829ca2e0e1
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-20 11:52:57 +0900

    chapter6-lesson12まで

--- chapter6/chapter6-lesson12/js/script.js
+++ chapter6/chapter6-lesson12/js/script.js
@@ -2,8 +2,7 @@ const items = document.querySelectorAll('.img-item');
 
 for(let i = 0; i < items.length; i++) {
   const keyframes = {
-    opacity: [0, 1],
-    rotate: ['x 90deg', 0],
+    filter: ['blur(20px)', 'blur(0)'],
   };
   const options = {
     duration: 600,
