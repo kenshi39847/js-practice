15609ccb58dcccc1cd215750c8bfdcb2508a2253
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-22 12:46:39 +0900

    chapter7-lesson3まで

---
+++ chapter7/Fashion/js/script.js
@@ -0,0 +1,32 @@
+/*
+ローディングから画面遷移
+================================================ */
+const loadingAreaGrey = document.querySelector('#loading');
+const loadingAreaGreen = document.querySelector('#loading-screen');
+
+window.addEventListener('load', () => {
+  //ローディング中（グレースクリーン）
+  loadingAreaGrey.animate({
+    opacity: [1, 0],
+    visibility: 'hidden'
+  },
+  {
+    duration: 2000,
+    delay: 1200,
+    easing: 'ease',
+    fill: 'forwards',
+  }
+  );
+  //ローディング中（薄緑スクリーン）
+  loadingAreaGreen.animate(
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh']
+    },
+    {
+      duration: 2000,
+      delay: 800,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
