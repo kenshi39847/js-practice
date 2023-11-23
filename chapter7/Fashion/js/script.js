4780137d3b209ba89c5091a427457c6f04fa1c25
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-23 19:40:24 +0900

    chapter7-lesson11途中

--- chapter7/Fashion/js/script.js
+++ chapter7/Fashion/js/script.js
@@ -87,11 +87,29 @@ const menuOptions = {
 
 //メニューを開く
 menuOpen.addEventListener('click', () => {
-  console.log('メニューを開く');
   menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+  //リンクを1つずつ順に表示
+  menuItems.forEach((menuItem, index) => {
+    console.log(`${index}番目のリスト`);
+    menuItem.animate(
+      {
+        opacity: [0, 1],
+        translate: ['2rem', 0],
+      },
+      {
+        duration: 2400,
+        delay: 300 * index,
+        easing: 'ease',
+        fill: 'forwards',
+      }
+    );
+  });
 });
 
 //メニューを閉じる
 menuClose.addEventListener('click', () => {
   menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+  menuItems.forEach((menuItem) => {
+    menuItem.animate({opacity: [1, 0]}, menuOptions);
+  });
 });
