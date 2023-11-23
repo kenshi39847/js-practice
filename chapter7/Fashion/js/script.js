c0c7e645686515757db5a0e31ae7d4264cc35268
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-23 09:37:51 +0900

    chapter7-lesson6まで

--- chapter7/Fashion/js/script.js
+++ chapter7/Fashion/js/script.js
@@ -49,3 +49,17 @@ window.addEventListener('load', () => {
     }
   );
 });
+
+/*
+画像ギャラリー
+================================================ */
+const mainImage = document.querySelector('.gallery-image img');
+const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
+
+for(let i = 0; i < thumbImages.length; i++) {
+  thumbImages[i].addEventListener('mouseover', () => {
+    mainImage.animate({opacity: [0, 1]}, 500);
+  });
+}
+
+
