0159c4b7a934b276d6819293db95e4dc779c1222
kenshi39847 <kenshi.t.blue@gmail.com>
2023-11-24 19:50:41 +0900

    chapter7-lesson13まで

--- chapter7/Fashion/js/script.js
+++ chapter7/Fashion/js/script.js
@@ -113,3 +113,17 @@ menuClose.addEventListener('click', () => {
     menuItem.animate({opacity: [1, 0]}, menuOptions);
   });
 });
+
+//監視対象が範囲内に現れたら実行する動作
+const animateFade = () => {
+  console.log('ふわ');
+};
+
+//監視設定
+const fadeObserver = new IntersectionObserver(animateFade);
+
+//.fadeinを監視するよう指示
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
+});
